24e56093b07a2f1e483791f2cf5e5ee1e93a3436
Akhil <email@example.com>
2024-01-20 11:38:33 +0530

    Added screen , icon and caption

--- Germ_invaders_Main.py
+++ Germ_invaders_Main.py
@@ -1,3 +1,12 @@
 import pygame
 
 pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 640
+
+icon = pygame.image.load("icon.png")
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption("Germ Invaders")
+pygame.display.set_icon(icon)
